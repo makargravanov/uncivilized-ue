eb62709d71fac1964da2e6f1cd600e4ea0583eb5
Alex <makargravanov@gmail.com>
2025-03-28 22:55:31 +0300

    add files

---
+++ Source/Uncivilized/Uncivilized.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Uncivilized.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Uncivilized, "Uncivilized" );
