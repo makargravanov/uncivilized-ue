4919a7c2a2a61ddb3994becb5ae062921f0b01f1
Alex <makargravanov@gmail.com>
2025-03-29 06:28:36 +0300

    update gitignore

--- Source/Uncivilized/Unreal/Environment/TileManager.cpp
+++ Source/Uncivilized/Unreal/Environment/TileManager.cpp
@@ -1,7 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "Unreal/Environment/TileManager.h"
+#include "TileManager.h"
 
 // Sets default values
 ATileManager::ATileManager()
